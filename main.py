0ddb59f50ea842cd8ff10b0c27f0d4165d80b1d3
jim <windachae47@gmail.com>
2022-05-21 21:54:45 +0300

    Вернул функцию метода Ричардсона

--- main.py
+++ main.py
@@ -33,6 +33,11 @@ print(exact)
 #    трём равномерно распределённым узлам x_1 = a, x_2 = (a + b)/2, x_3 = b.
 #    Оценить методическую погрешность построенного правила (11), сравнить её с точной погрешностью.
 
+# Осталось сделать
+#   1. Реализовать аналитическое вычисление весовой функции
+#   2. Оценить методическую погрешность
+#   3. Сравнить методическую погрещность с точной
+
 x1 = a
 x2 = (a + b) / 2
 x3 = b
@@ -58,16 +63,9 @@ def newton_cotes(p_func=p, N_: int = 3, h_: int = -1,
     A = [np.power(nodes_x, i) for i in range(0, N_)]
     return np.linalg.solve(A, mU)
 
-
-
-
-
-exact, *_ = integrate.quad(func=F, a=a, b=b)
-
 N = 3;
 x_ = np.linspace(a, b, N)
-# An = newton_cotes(N_=N)
-An = Gauss(N_=N)
+An = newton_cotes(N_=N)
 quad = np.sum(An * f(x_))
 error = abs(quad - exact)
 print('{:2d}  {:10.9f}  {:.5e}'.format(N, quad, error))
@@ -79,6 +77,8 @@ print('{:2d}  {:10.9f}  {:.5e}'.format(N, quad, error))
 # Погрешность оценивать методом Ричардсона. На каждых по-
 # следовательных трёх сетках оценивать скорость сходимости по
 # правилу Эйткена.
+
+
 def Aitken_process(h__: float = abs(b - a) / 3, L: float = 2, a_: float = a, b_: float = b):
     h1 = h__
     h2 = h__ / L
@@ -92,14 +92,40 @@ def Aitken_process(h__: float = abs(b - a) / 3, L: float = 2, a_: float = a, b_:
     m = -(np.log((S_h3 - S_h2) / (S_h2 - S_h1)) / np.log(L))
     return (m)
 
+
+def Richardson(h__: float=abs(b - a) / 3, method: str ='newton_cotes', r: int=4):
+    """
+    Parameters
+    ----------
+    :param h__: float
+        величина шага
+    :param method: str =
+        ипользуемый метод оценки == 'newton_cotes' || 'gauss'
+    :param r: int
+        степень разложения
+    :return: list
+    """
+    result = list()
+    return result
+
 # Task 1.3
 # Проведя вычисления по трём грубым сеткам с малым числом
 # шагов (например, 1, 2 и 4) использовать оценку скорости сходи-
-# мости и выбрать оптимальный шаг h opt . Начать расчёт c шага
-# h opt и снова довести до требуемой точности ε.
+# мости и выбрать оптимальный шаг h opt .
+# Начать расчёт c шага h_opt и снова довести до требуемой точности ε.
 
 
-# Task
+# Вариант Гаусса
+# Выполнить всё то же самое, используя трёхточечные формулы Гаусса вместо формул Ньютона — Котса. Узлы каждой малой формулы
+# находить либо с помощью формул Кардано, либо численно.
+#
+# Замечание:
+#   Обратите внимание, что из-за ограниченности разрядной сетки
+#   при хранении чисел и большой чувствительности полиномов к по-
+#   грешностям в их коэффициентах, может оказаться так, что узлы
+#   формул Гаусса, находимые как корни узлового многочлена, будут
+#   выходить за границы отрезка интегрирования, что не позволит най-
+#   ти с их помощью решение задачи.
 
 def Gauss(p_func=p, N_: int = 3,
           a_: float = a, b_: float = b):
@@ -123,3 +149,11 @@ def Gauss(p_func=p, N_: int = 3,
     x_j = np.roots(a_i_j.transpose())
     A = np.array([np.power(x_j, i) for i in range(0, N_)])
     return np.linalg.solve(A, mU[0:N_])
+
+
+N = 3
+x_ = np.linspace(a, b, N)
+An = Gauss(N_=N)
+quad = np.sum(An * f(x_))
+error = abs(quad - exact)
+print('{:2d}  {:10.9f}  {:.5e}'.format(N, quad, error))
