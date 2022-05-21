b4d54b6b66a06be753ea406204db1a71c50faf80
jim <windachae47@gmail.com>
2022-05-21 21:36:17 +0300

    Внёсс изменения Ромы, добавил константу таргет и переформатировал файл комментарии в целом

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 from scipy import integrate
-from scipy.special import gammaln
 
 # Степени и пределы интегрирования
 alpha = 1 / 5
@@ -28,23 +27,27 @@ def F(x: np.float_) -> np.float_:
 exact, err = integrate.quad(func=F, a=a, b=b)
 print(exact)
 
+# Task 1.1 (Вариант Ньютона-Котса)
+#    Построить интерполяционную квадратурную формулу с весо-
+#    вой функцией p(x) = (x − a)^(−α) (b − x)^(−β) на отрезке [a, b] по
+#    трём равномерно распределённым узлам x_1 = a, x_2 = (a + b)/2, x_3 = b.
+#    Оценить методическую погрешность построенного правила (11), сравнить её с точной погрешностью.
 
-# Task 1.1
-# Построить интерполяционную квадратурную формулу с весо-
-# вой функцией p(x) = (x − a)^(−α) (b − x)^(−β) на отрезке [a, b] по
-# трём равномерно распределённым узлам x_1 = a, x_2 = (a + b)/2,
-# x_3 = b. Оценить методическую погрешность построенного пра-
-# вила (11), сравнить её с точной погрешностью.
+x1 = a
+x2 = (a + b) / 2
+x3 = b
+
+TARGET = 3.578861536040539915439859609644293194417  # Точное значение интеграла
 
 
 def newton_cotes(p_func=p, N_: int = 3, h_: int = -1,
                  a_: float = a, b_: float = b):
     mU = []
     # Задаём узлы квадратурной формулы
-    if N_ != -1:
+    if (N_ != -1):
         nodes_x = np.linspace(a_, b_, N_)
     else:
-        if h_ != -1:
+        if (h_ != -1):
             nodes_x = np.range(a_, b_, h_)
     # Вычисляем моменты весовой функции p(x) на [a,b]
     for i in range(0, N_):
@@ -56,30 +59,15 @@ def newton_cotes(p_func=p, N_: int = 3, h_: int = -1,
     return np.linalg.solve(A, mU)
 
 
-def Gauss(p_func=p, N_: int = 3, h_: int = -1,
-          a_: float = a, b_: float = b):
-    mU = []
-    # Задаём узлы квадратурной формулы
-    if N_ != -1:
-        nodes_x = np.linspace(a_, b_, N_)
-    else:
-        if h_ != -1:
-            nodes_x = np.range(a_, b_, h_)
-    # Вычисляем моменты весовой функции p(x) на [a,b]
-    for i in range(0, 2 * N_ - 1):
-        v, *_ = integrate.quad(func=lambda x_: p_func(x_) * np.power(x_, i), a=a_, b=b_)
-        mU.append(v)
-    # Решаем СЛАУ
-    mU = np.array(mU)
-    A = [np.power(nodes_x, i) for i in range(0, N_)]
-    return np.linalg.solve(A, mU)
+
 
 
 exact, *_ = integrate.quad(func=F, a=a, b=b)
 
-N = 3
+N = 3;
 x_ = np.linspace(a, b, N)
-An = newton_cotes(N_=N)
+# An = newton_cotes(N_=N)
+An = Gauss(N_=N)
 quad = np.sum(An * f(x_))
 error = abs(quad - exact)
 print('{:2d}  {:10.9f}  {:.5e}'.format(N, quad, error))
@@ -91,7 +79,6 @@ print('{:2d}  {:10.9f}  {:.5e}'.format(N, quad, error))
 # Погрешность оценивать методом Ричардсона. На каждых по-
 # следовательных трёх сетках оценивать скорость сходимости по
 # правилу Эйткена.
-
 def Aitken_process(h__: float = abs(b - a) / 3, L: float = 2, a_: float = a, b_: float = b):
     h1 = h__
     h2 = h__ / L
@@ -103,23 +90,7 @@ def Aitken_process(h__: float = abs(b - a) / 3, L: float = 2, a_: float = a, b_:
     S_h2 = np.sum(newton_cotes(h_=h2) * f(x_2))
     S_h3 = np.sum(newton_cotes(h_=h3) * f(x_3))
     m = -(np.log((S_h3 - S_h2) / (S_h2 - S_h1)) / np.log(L))
-    return m
-
-def Richardson(h__: float=abs(b - a) / 3, method: str ='newton_cotes', r: int=4):
-    """
-    Parameters
-    ----------
-    :param h__: float
-        величина шага
-    :param method: str =
-        ипользуемый метод оценки == 'newton_cotes' || 'gauss'
-    :param r: int
-        степень разложения
-    :return: list
-    """
-    result = list()
-    return result
-    # Коэффициенты C_i мы находим как A_i? Или как?
+    return (m)
 
 # Task 1.3
 # Проведя вычисления по трём грубым сеткам с малым числом
@@ -128,3 +99,27 @@ def Richardson(h__: float=abs(b - a) / 3, method: str ='newton_cotes', r: int=4)
 # h opt и снова довести до требуемой точности ε.
 
 
+# Task
+
+def Gauss(p_func=p, N_: int = 3,
+          a_: float = a, b_: float = b):
+    mU = []
+
+    # 1 Вычисляем моменты весовой функции p(x) на [a,b]
+    for i in range(0, 2 * N_):
+        v, *_ = integrate.quad(func=lambda x_: p_func(x_) * np.power(x_, i), a=a_, b=b_)
+        mU.append(v)
+
+    mU_n_plus_s = map(lambda x: -x, mU[N_:2 * N_])
+    # 2 Решаем СЛАУ
+    mU_j_plus_s = np.zeros((N_, N_))
+    for i in range(0, N_):
+        for j in range(0, N_):
+            mU_j_plus_s[i, j] = mU[i + j]
+
+    a_i_j = np.linalg.solve(mU_j_plus_s, mU_n_plus_s)
+    # Находим узлы, как корни узлового многочлена
+    # Добавить единичку в a_i_j
+    x_j = np.roots(a_i_j.transpose())
+    A = np.array([np.power(x_j, i) for i in range(0, N_)])
+    return np.linalg.solve(A, mU[0:N_])
